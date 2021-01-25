3453258f958eca92d0ce59e10c4ace3d56a27e07
alexsoler <alejosod@gmail.com>
2021-01-25 17:13:02 -0600

    create CreateTodo DTO. Add class-validator class create todo entity added database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './todo/todo.module';
+
+const dataBaseModule = TypeOrmModule.forRoot({
+  autoLoadEntities: true,
+  database: 'test',
+  entities: [],
+  host: 'localhost',
+  password: 'root',
+  port: 3306,
+  synchronize: true,
+  type: 'mysql',
+  username: 'root',
+});
 
 @Module({
-  imports: [],
+  imports: [dataBaseModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/todo/todo.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  active: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
