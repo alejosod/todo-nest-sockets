1c8d0e724406b2dedc8e17f19537250efe29d2aa
alexsoler <alejosod@gmail.com>
2021-01-25 18:30:40 -0600

    added .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 import { Todo } from './todo/todo.entity';
+import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const dataBaseModule = TypeOrmModule.forRoot({
   autoLoadEntities: true,
-  database: 'test',
+  database: process.env.DB_NAME,
   entities: [Todo],
-  host: 'localhost',
-  password: 'Ichthysabba2',
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
   port: 3306,
   type: 'mysql',
-  username: 'root',
+  username: process.env.DB_USERNAME,
 });
 
 @Module({
-  imports: [dataBaseModule, TodoModule],
+  imports: [ConfigModule.forRoot(), dataBaseModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
